c6c7ddc0943661d0d9080abeb4ce2be2114a5cc4
ana rusconi <anarusconi3@gmail.com>
2023-10-25 12:59:53 -0300

    modelos,controladores,migraciones

---
+++ database/migrations/2023_10_25_132206_create_productos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Categoria;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombre",255);
+            $table->double("precio");
+            $table->string("imagen",255);
+            $table->string("descripcion",255);
+
+            $table->boolean("habilitado");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
---
+++ database/migrations/2023_10_25_132309_create_ordens_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function Laravel\Prompts\table;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ordens', function (Blueprint $table) {
+            $table->id();
+            $table->carritos_id("carritos.id");
+            $table->metodos_pago_id("metodos_pago.id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ordens');
+    }
+};
